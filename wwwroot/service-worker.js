c45bebdbee33d47f64a7c74553580e0b15a7eb6b
omarmohamed <os48912@gmail.com>
2026-04-30 17:35:49 +0300

    Update Version 1.0.0

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -47,7 +47,8 @@ async function onFetch(event) {
         // unless that request is for an offline resource.
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !manifestUrlList.some(url => url === event.request.url);
+            && !manifestUrlList.some(url => url === event.request.url)
+            && new URL(event.request.url).pathname.startsWith(base);
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
